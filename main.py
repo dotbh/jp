5a711cd2b7602be6490e30a2efe54afda9fa7b2c
dotbh <161698426+dotbh@users.noreply.github.com>
2024-04-11 10:58:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 
-api_url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json'
+api_url = 'https://api.adviceslip.com/advice'
 
 response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
 
 
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    insult = response.json()['insult']
-    print(insult)
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе 
+    slip = response.json()['slip']
+    res_data = slip['advice']
+    print(res_data)
 else:
     print(response.status_code)
